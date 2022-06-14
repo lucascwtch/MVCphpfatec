721e2080938dbfc91719b28d248e3e6d5c287c3a
Lucas Almeida <lucas.cwtch@gmail.com>
2022-06-14 11:09:33 -0300

    correção 14/06

--- model/banco.php
+++ model/banco.php
@@ -2,17 +2,23 @@
 require_once('init.php');
 
 class Banco{
-    protected $mysqli;
-
-    public function _construct(){
+    private $mysqli;
+    
+    public function __construct(){
         echo "Conexão efetuada com sucesso";
         $this->conexao();
     }
-    
-    private function conexao(){
-        $this->mysqli = new mysqli(BD_SERVIDOR, BD_USUARIO, BD_SENHA, BD_BANCO);
-    }   
 
+    public function query($sql){
+	$this->stmt = $this->mysqli->prepare($sql);
+    }
+
+    public function conexao(){
+        $this->mysqli = new mysqli(BD_SERVIDOR, BD_USUARIO, BD_SENHA, BD_BANCO);
+        if ($this->mysqli->connect_errno) {
+            echo "Falha ao conectar ao MySQL: (" . $this->mysqli->connect_errno . ") " . $this->mysqli->connect_error;
+    } 
+}
     public function setJogo($nome,$programador,$quantidade_niveis,$preco,$data){
         $stmt = $this->mysqli->prepare("INSERT INTO jogos (`nome`,`programador`,`quantidade_niveis`,`preco`,`data`) VALUES (?,?,?,?,?)");
         $stmt->bind_param("sssss",$nome,$programador,$quantidade_niveis,$preco,$data);
--- model/init.php
+++ model/init.php
@@ -3,6 +3,6 @@ date_default_timezone_set('America/Sao_Paulo');
 
 define('BD_SERVIDOR','localhost');
 define('BD_USUARIO','id18992157_lucascw');
-define('BD_SENHA','+bFrb&4M\&odN]RZ');
+define('BD_SENHA','49QeU|]/t|ss{0!r');
 define('BD_BANCO','id18992157_jogaria');
 ?>
